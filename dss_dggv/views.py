778c5eafc4f843261ffc052a882fdf5c39e2b3ed
Hoangi19 <viethoanghy99@gmail.com>
2020-12-16 00:37:13 +0700

    add example query

--- dss_dggv/views.py
+++ dss_dggv/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+from  .models import Giangvien
+
+def gv_detail_view(request):
+
+    return HttpResponse();
+
+def index(request):
+    object = Giangvien.objects.filter(ma_nganh="MAT130");
+    print(object[0].ma_nganh);
+    return HttpResponse("Hello, world. You're at the polls index.")
