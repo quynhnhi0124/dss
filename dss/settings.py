bbeccf0e2b49b7c03f643058be379658fba92a71
Minh <hnim97@gmail.com>
2020-12-19 19:53:02 +0700

    sua giao dien

--- dss/settings.py
+++ dss/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import pyodbc  
+# import pyodbc  
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'dss.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'sql_server.pyodbc',
+        # 'ENGINE': 'sql_server.pyodbc',
         'NAME': 'dss',
         'USER':'dssadmin',
         'PASSWORD':'Dss@1234',
