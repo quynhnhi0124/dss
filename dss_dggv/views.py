711acfa255e2df133b9b81de0f29631651f421c0
hoangi19 <viethoanghy99@gmail.com>
2021-01-01 11:02:40 +0700

    hiển thị danh sách gv

--- dss_dggv/views.py
+++ dss_dggv/views.py
@@ -21,7 +21,7 @@ def gv_detail_view(request):
     return HttpResponse();
 
 def index(request):
-    object = Giangvien.objects.filter(ma_nganh="MAT130").values('ten', 'ma_nganh');
-    res = json.dumps(list(object), ensure_ascii=False).encode('utf8')
-    print(res.decode())
-    return render(request, "pages/base.html", {})
+    object = Giangvien.objects.filter(ma_nganh="MAT130").values('ten', 'nam_sinh', 'dia_chi', 'gioi_tinh', 'ma_truong', 'tn_dh', 'tn_ch', 'hoc_vi', 'hoc_ham', 'nam_tn_dh', 'nam_tn_ch', 'nam_tn_ts', 'tn_ts')
+    # res = json.dumps(list(object), ensure_ascii=False).encode('utf8')
+    # print({'list' : list(object)})
+    return render(request, "pages/base.html", {'giaovien' : list(object)})
